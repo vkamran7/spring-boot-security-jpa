bd8b9c5e31ddc4fb87f53de4aeffcfdfcf474c77
Kamran <vkamran7@gmail.com>
2020-05-12 13:06:41 +0400

    SpringBoot Security+JPA

---
+++ src/main/java/com/secureapp/MyUserDetails.java
@@ -0,0 +1,53 @@
+package com.secureapp;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class MyUserDetails implements UserDetails {
+    private String userName;
+
+    public MyUserDetails(String userName) {
+        this.userName = userName;
+    }
+
+    public MyUserDetails() {
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return "pass";
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
